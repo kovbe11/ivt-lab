2a6e30fb5475b72d3c92b2d609f2c30b41770501
Kovacs Bence <bence.kovacs.zoltan@gmail.com>
2021-05-16 19:50:38 +0200

    implementing additional test cases

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
@@ -49,6 +50,126 @@ public class GT4500Test {
 
         verify(primary, times(1)).fire(1);
         verify(secondary, times(1)).fire(1);
+    }
+
+    /*
+    This is the same test as single success, it's here to show that I planned 5 tests
+    @Test
+    public void fireTorpedoSingle_Primary_Store_Fired_First(){
+        fireTorpedo_Single_Success();
+    }*/
+
+    @Test
+    public void fireTorpedoSingle_Torpedoes_Are_Fired_Alternating(){
+        // Arrange
+        when(primary.fire(1)).thenReturn(true);
+        when(secondary.fire(1)).thenReturn(true);
+
+        // Act
+        boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+        boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+        // Assert
+        assertTrue(result1);
+        assertTrue(result2);
+
+        verify(primary, times(1)).fire(1);
+        verify(secondary, times(1)).fire(1); //alternating
+    }
+
+    @Test
+    public void fireTorpedoSingle_If_Store_Is_Empty_Fire_Other_Store(){
+        // Arrange
+        when(primary.isEmpty()).thenReturn(true);
+        when(secondary.isEmpty()).thenReturn(false);
+
+        when(primary.fire(1)).thenThrow(new IllegalArgumentException());
+        when(secondary.fire(1)).thenReturn(true);
+
+        // Act
+        boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+        // Assert
+
+        assertTrue(result);
+
+        verify(primary, times(0)).fire(1);
+        verify(secondary, times(1)).fire(1);
+    }
+
+    @Test
+    public void fireTorpedoSingle_If_Error_Doesnt_Fire_Other_Store(){
+        when(primary.fire(1)).thenThrow(new IllegalArgumentException());
+        when(secondary.fire(1)).thenReturn(true);
+
+        // Act
+        boolean result;
+        try{
+            result = ship.fireTorpedo(FiringMode.SINGLE);
+        }catch (IllegalArgumentException exception){
+            result = false;
+        }
+
+        // Assert
+        assertFalse(result);
+
+        verify(primary, times(1)).fire(1);
+        verify(secondary, times(0)).fire(1);
+    }
+
+    @Test
+    public void fireTorpedoAll_Fires_Both_Torpedoes(){
+        // Arrange
+        when(primary.fire(1)).thenReturn(true);
+        when(secondary.fire(1)).thenReturn(true);
+
+        // Act
+        boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+        // Assert
+        assertTrue(result);
+
+        verify(primary, times(1)).fire(1);
+        verify(secondary, times(1)).fire(1);
+    }
+
+    @Test
+    public void fireTorpedoAll_Try_Firing_Both_But_One_Is_Empty(){
+        // Arrange
+        when(primary.isEmpty()).thenReturn(true);
+        when(secondary.isEmpty()).thenReturn(false);
+
+        when(primary.fire(1)).thenThrow(new IllegalArgumentException());
+        when(secondary.fire(1)).thenReturn(true);
+
+        // Act
+        boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+        // Assert
+        assertTrue(result);
+
+        verify(primary, times(0)).fire(1);
+        verify(secondary, times(1)).fire(1);
+    }
+
+    @Test
+    public void fireTorpedoSingle_Fire_Twice_Secondary_Is_Empty_First_Fires_Again(){
+        when(primary.isEmpty()).thenReturn(false);
+        when(secondary.isEmpty()).thenReturn(true);
+
+        when(primary.fire(1)).thenReturn(true);
+        when(secondary.fire(1)).thenThrow(new IllegalArgumentException());
+
+        // Act
+        boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+        boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+        // Assert
+        assertTrue(result1);
+        assertTrue(result2);
+
+        verify(primary, times(2)).fire(1);
+        verify(secondary, times(0)).fire(1);
     }
 
 }
